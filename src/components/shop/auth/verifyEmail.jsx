e01bafd5cc5b58a5599f3220448e91d5f29114de
Hundaol Negusie <hundaolnk@gmail.com>
2022-11-26 08:10:41 +0300

    corrected reset password

--- src/components/shop/auth/verifyEmail.jsx
+++ src/components/shop/auth/verifyEmail.jsx
@@ -6,42 +6,42 @@ import styles from "./styles.module.css";
 // import { Fragment } from "react/cjs/react.production.min";
 
 const VerifyEmail = () => {
-	const [validUrl, setValidUrl] = useState(true);
-	const param = useParams();
+  const [validUrl, setValidUrl] = useState(true);
+  const param = useParams();
 
-	useEffect(() => {
-		const verifyEmailUrl = async () => {
-			try {
-				const url_F = `http://localhost:8000/api/${param.id}/verify/${param.token}`;
-				const url = `https://cute-cyan-tuna-slip.cyclic.app/api/${param.id}/verify/${param.token}`;
-				const { data } = await axios.get(url);
-				console.log(data);
-				setValidUrl(true);
-			} catch (error) {
-				console.log(error);
-				setValidUrl(false);
-			}
-		};
-		verifyEmailUrl();
-	}, [param]);
+  useEffect(() => {
+    const verifyEmailUrl = async () => {
+      try {
+        const url = `http://localhost:8000/api/${param.id}/verify/${param.token}`;
+        const url_F = `https://cute-cyan-tuna-slip.cyclic.app/api/${param.id}/verify/${param.token}`;
+        const { data } = await axios.get(url);
+        console.log(data);
+        setValidUrl(true);
+      } catch (error) {
+        console.log(error);
+        setValidUrl(false);
+      }
+    };
+    verifyEmailUrl();
+  }, [param]);
 
-	return (
-		// <Fragment>
+  return (
+    // <Fragment>
     <>
-			{validUrl ? (
-				<div className={styles.container}>
-					<img src="success" alt="success_img" className={styles.success_img} />
-					<h1>Email verified successfully</h1>
-					<Link to="/login">
-						<button className={styles.green_btn}>Login</button>
-					</Link>
-				</div>
-			) : (
-				<h1>404 Not Found</h1>
-			)}
-		{/* </Fragment> */}
+      {validUrl ? (
+        <div className={styles.container}>
+          <img src="success" alt="success_img" className={styles.success_img} />
+          <h1>Email verified successfully</h1>
+          <Link to="/login">
+            <button className={styles.green_btn}>Login</button>
+          </Link>
+        </div>
+      ) : (
+        <h1>404 Not Found</h1>
+      )}
+      {/* </Fragment> */}
     </>
-	);
+  );
 };
 
 export default VerifyEmail;
